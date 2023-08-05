cea6430d2cf8fc5843e3bd0e31074e1d38a2e1b9
Kseniia-Z <xeniyaagl@gmail.com>
2023-08-05 12:50:47 +0300

    the second commit

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,8 +1,9 @@
 let timer = document.getElementById("timer");
-const TimerInterval = setInterval(() => {
+const timerInterval = setInterval(() => {
     if(timer.textContent > 0) {
-        timer.textContent --
+        timer.textContent --;
     } else {
         alert("Вы победили в конкурсе");
+        clearInterval(timerInterval);
     }
 },1000);
